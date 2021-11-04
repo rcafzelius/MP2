958056a616d7adab6984b73e0d8ebd2e35b0c722
Roland Afzelius <afzelius@bc.edu>
2021-11-04 00:16:20 -0400

    More of an outline

--- main.go
+++ main.go
@@ -8,23 +8,17 @@ import (
 	"time"
 )
 
-
-
-
 type Block struct {
-	name, puzzle int
-
+	name, hash, solution int
 }
 
-//
 type Node struct {
 	//Could store blocks in a map or in a linked list --
 	blocks map[int]Block
-	solution map[int]int
-
-	sendBlock map[int]chan Block
 }
 
+var logToNode map[int]chan Node
+var nodeToLog chan int
 
 
 //Checks validity of block
@@ -35,6 +29,21 @@ func logger(){
 //Updates nodes when a new valid block is added
 func updateNodes() {
 
+}
+
+//checkStatus after each iteration in solver check for an update of chain
+func checkStatus() {
+
+}
+
+//Take in Node and channel to send a solution
+func solver(){
+
+}
+
+//Run Go Routines to have nodes run a mining function "solver" at the same time
+func mine() {
+	for
 }
 
 func main() {
